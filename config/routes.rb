f6d9ef3857b5f3996119eb15f917b3b5ac61967e
Lin <mccartneylc@gmail.com>
2016-07-26 15:57:00 -0600

    Add vanity url for categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  resources :categories, only: [:index, :show]
+  resources :categories, only: [:index]
 
   resources :animals, only: [:show, :index]
 
   resources :cart_animals, only: [:create]
 
   get '/cart', to: 'cart_animals#index'
+
+  get "/:category_slug", to: 'categories#show'
+
 end
