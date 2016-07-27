735326b4215b70cb6b213a17d39e2c5e54e05880
Lin <mccartneylc@gmail.com>
2016-07-27 08:02:34 -0600

    Add validations on Animal and Category Add model tests for Animal and Category Adjust feature tests missing price and image_url for Animal

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'rails_12factor', group: :production
 group :test do
   gem 'simplecov'
   gem 'launchy'
+  gem 'shoulda-matchers', '~> 3.1'
 end
 
 group :development, :test do
--- spec/features/user_can_see_animals_spec.rb
+++ spec/features/user_can_see_animals_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.feature "UserCanSeeAnimals", type: :feature do
   scenario "within a specific category" do
     category = Category.create(name: "Big Cats")
-    category.animals.create(name: "Tiger")
-    category.animals.create(name: "Lion")
+    category.animals.create(name: "Tiger", description: "Stalker in the night", price: 3500, image_path: "http://wildaid.org/sites/default/files/photos/iStock_000008484745Large%20%20tiger%20-%20bengal.jpg")
+    category.animals.create(name: "Lion", description: "Lazy during the day", price: 7000, image_path: "http://wildaid.org/sites/default/files/photos/iStock_000008484745Large%20%20tiger%20-%20bengal.jpg")
 
     visit '/big-cats'
 
@@ -14,7 +14,7 @@ RSpec.feature "UserCanSeeAnimals", type: :feature do
 
   scenario "they see animal name and description after clicking on animal link" do
     category = Category.create(name: "Big Cats")
-    animal = category.animals.create(name: "Tiger", description: "Stalker in the night")
+    animal = category.animals.create(name: "Tiger", description: "Stalker in the night", price: 3500, image_path: "http://wildaid.org/sites/default/files/photos/iStock_000008484745Large%20%20tiger%20-%20bengal.jpg")
 
     visit '/big-cats'
 
@@ -27,7 +27,7 @@ RSpec.feature "UserCanSeeAnimals", type: :feature do
 
   scenario "they see index of all animals" do
     category = Category.create(name: "Big Cats")
-    animal = category.animals.create(name: "Tiger", description: "Stalker in the night", image_path: "http://wildaid.org/sites/default/files/photos/iStock_000008484745Large%20%20tiger%20-%20bengal.jpg")
+    animal = category.animals.create(name: "Tiger", description: "Stalker in the night", price: 3500, image_path: "http://wildaid.org/sites/default/files/photos/iStock_000008484745Large%20%20tiger%20-%20bengal.jpg")
 
     visit categories_path
 
