488e711e0484b3e00e0a40b29f8e4c807b6e5fef
matthewrpacker <matthewrpacker@gmail.com>
2016-07-27 18:36:02 -0600

    Modify dashboard path to remove id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      redirect_to dashboard_path(id: user.id)
+      redirect_to dashboard_path
     else
       flash.now[:failure] = "Invalid login"
       render :new
