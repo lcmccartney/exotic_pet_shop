21c519d7d7a1e741292fbfa37c6c1f4398719c4b
David Tinianow <david.tinianow@gmail.com>
2016-07-29 20:11:17 -0600

    User can see retired items

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160730004920) do
+ActiveRecord::Schema.define(version: 20160730014225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,10 +20,11 @@ ActiveRecord::Schema.define(version: 20160730004920) do
     t.string   "name"
     t.text     "description"
     t.integer  "category_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
     t.string   "image_path"
     t.integer  "price"
+    t.integer  "status",      default: 0
   end
 
   add_index "animals", ["category_id"], name: "index_animals_on_category_id", using: :btree
--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -5,4 +5,17 @@ RSpec.describe Animal, type: :model do
   it { should validate_presence_of(:description) }
   it { should validate_presence_of(:price) }
   it { should validate_presence_of(:image_path) }
+
+  it "should check whether animal is extinct" do
+    category = Category.create(name: "Big Cats")
+    animal_1 = category.animals.create(name: "Tiger", status: 1, description: "Stalker in the night", price: 3500, image_path: "http://wildaid.org/sites/default/files/photos/iStock_000008484745Large%20%20tiger%20-%20bengal.jpg")
+
+    extinct_animal = animal_1.extinct?
+    expect(extinct_animal).to eq(true)
+    
+    animal_2 = category.animals.create(name: "Tiger", status: 0, description: "Stalker in the night", price: 3500, image_path: "http://wildaid.org/sites/default/files/photos/iStock_000008484745Large%20%20tiger%20-%20bengal.jpg")
+
+    active_animal = animal_2.extinct?
+    expect(active_animal).to eq(false)
+  end
 end
