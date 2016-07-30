622c5475a30c52a8ce421b340a4d0b66a7de29bd
David Tinianow <david.tinianow@gmail.com>
2016-07-29 20:17:17 -0600

    Fix hound messages

--- spec/features/user_can_see_retired_items_spec.rb
+++ spec/features/user_can_see_retired_items_spec.rb
@@ -1,13 +1,19 @@
 require 'rails_helper'
 
 RSpec.feature "UserCanSeeRetiredItems", type: :feature do
-   scenario "they can see animal page but cannot add to cart" do
-     category = Category.create(name: "Big Cats")
-     animal = category.animals.create(name: "Tiger", status: 1, description: "Stalker in the night", price: 3500, image_path: "http://wildaid.org/sites/default/files/photos/iStock_000008484745Large%20%20tiger%20-%20bengal.jpg")
+  scenario "they can see animal page but cannot add to cart" do
+    category = Category.create(name: "Big Cats")
+    animal = category.animals.create(
+     name: "Tiger",
+     status: 1,
+     description: "Stalker in the night",
+     price: 3500,
+     image_path: "http://wildaid.org/sites/default/files/photos/iStock_000008484745Large%20%20tiger%20-%20bengal.jpg"
+     )
 
-     visit "/animals/#{animal.id}"
+    visit "/animals/#{animal.id}"
 
-     expect(page).to have_no_content("Add to Cart")
-     expect(page).to have_content("Extinct")
-   end
+    expect(page).to have_no_content("Add to Cart")
+    expect(page).to have_content("Extinct")
+  end
 end
