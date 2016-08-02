11289a376efdc82bba1baa671c4f69ad11b93066
David Tinianow <david.tinianow@gmail.com>
2016-08-02 16:43:29 -0600

    Mailer (#112)
    * Add default image path for animal
    
    * Add email confirmation for sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      UserNotifier.send_signup_email(@user).deliver if @user.email.present?
       flash[:success] = "You have successfully created a new account."
       redirect_to cart_path if @cart.contents.any?
       redirect_to dashboard_path if @cart.contents.empty?
@@ -29,6 +30,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:username, :password, :email)
   end
 end
---
+++ app/mailers/user_notifier.rb
@@ -0,0 +1,9 @@
+class UserNotifier < ActionMailer::Base
+  default from: 'thebeast@fancybeast.io'
+
+  def send_signup_email(user)
+    @user = user
+    mail(to: @user.email,
+    subject: 'Welcome to Fancy Beast')
+  end
+end
