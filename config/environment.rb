128b296a62f36e772bef0737b2c5e3f760655edb
David Tinianow <david.tinianow@gmail.com>
2016-08-02 16:15:55 -0600

    Add email confirmation for sign-up

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'fancybeast',
+  :password => 'mod21605',
+  :domain => 'fancybeast.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
