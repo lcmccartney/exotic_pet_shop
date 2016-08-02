72114b69b94ad338e72dd7b07297c478bc9f6146
David Tinianow <david.tinianow@gmail.com>
2016-08-02 15:18:37 -0600

    Add default image path for animal (#110)
    * Add default image path for animal

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -3,12 +3,16 @@ class Animal < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
   validates :price, presence: true
-  validates_numericality_of :price, greater_than: 0
   validates :image_path, presence: true
+  validates_numericality_of :price, greater_than: 0
   validates :category_id, presence: true
   enum status: %w[active extinct]
 
   belongs_to :category
   has_many :order_animals
   has_many :orders, through: :order_animals
+
+  def default_image
+    "https://s-media-cache-ak0.pinimg.com/564x/13/05/6d/13056dae918b65e94c7d36e885c30398.jpg"
+  end
 end
--- spec/features/admin_can_create_an_animals_spec.rb
+++ spec/features/admin_can_create_an_animals_spec.rb
@@ -35,4 +35,39 @@ RSpec.feature "AdminCanCreateAnAnimals", type: :feature do
     expect(page).to have_content('9,000')
     expect(animal.category.name).to eq("Big Cats")
   end
+
+  scenario "they leave image path blank and still see an image" do
+    user = User.create(role: 1, username: "admin", password: "password")
+    Category.create(name: "Big Cats")
+    Category.create(name: "Reptiles")
+
+    visit login_path
+
+    fill_in "Username", with: "admin"
+    fill_in "Password", with: "password"
+
+    click_button("Login")
+
+    expect(current_path).to eq(admin_dashboard_path)
+    click_on("Create an Animal")
+
+    expect(current_path).to eq(new_animal_path)
+    fill_in "Name", with: "Cougar"
+    fill_in "Description", with: "Active in the day"
+    fill_in "Price", with: 9000
+    select "Big Cats"
+    click_on "Create"
+    animal = Animal.last
+    expect(current_path).to eq(new_animal_path)
+    expect(page).to have_content("Successfully created #{animal.name}")
+
+    visit animal_path(animal)
+
+    expect(current_path).to eq(animal_path(animal))
+    expect(page).to have_content("Cougar")
+    expect(page).to have_content("Active in the day")
+    expect(page).to have_content('9,000')
+    expect(animal.category.name).to eq("Big Cats")
+    expect(page).to have_xpath("//img[@src='https://s-media-cache-ak0.pinimg.com/564x/13/05/6d/13056dae918b65e94c7d36e885c30398.jpg']")
+  end
 end
