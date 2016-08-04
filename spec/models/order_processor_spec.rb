fc78fb940a4513bd0a3b4c382436009d63665d01
David Tinianow <david.tinianow@gmail.com>
2016-08-03 18:24:40 -0600

    Fix hound errors

--- spec/models/order_processor_spec.rb
+++ spec/models/order_processor_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe OrderProcessor, type: :model do
   it "should create initalize with an order and cart" do
-   cart = Cart.new("1" => 2, "2" => 3)
-   order = Order.new(total_price: 1000)
-   op = OrderProcessor.new(order, cart)
+    cart = Cart.new("1" => 2, "2" => 3)
+    order = Order.new(total_price: 1000)
+    op = OrderProcessor.new(order, cart)
 
-   op_cart = op.cart
-   op_order = op.order
+    op_cart = op.cart
+    op_order = op.order
 
-   expect(op_cart).to be_a Cart
-   expect(op_order).to be_a Order
+    expect(op_cart).to be_a Cart
+    expect(op_order).to be_a Order
   end
 end
